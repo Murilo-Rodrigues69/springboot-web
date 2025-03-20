9bac7d23802d55940b68be98dc0383fd805c2a3e
Murilo Rodrigues <murilooliveira10789@gmail.com>
2025-03-20 17:04:23 -0300

    projected created

---
+++ src/main/java/com/ceramicamaster/aprender/AprenderApplication.java
@@ -0,0 +1,13 @@
+package com.ceramicamaster.aprender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprenderApplication.class, args);
+	}
+
+}
