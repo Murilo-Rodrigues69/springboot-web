17799743ea247451bf24fce6b946348c7bf39b98
Murilo Rodrigues <murilooliveira10789@gmail.com>
2025-03-25 18:07:19 -0300

    Service layer, component registration

---
+++ src/main/java/com/ceramicamaster/aprender/services/UserService.java
@@ -0,0 +1,25 @@
+package com.ceramicamaster.aprender.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ceramicamaster.aprender.entities.User;
+import com.ceramicamaster.aprender.repositories.UserRepository;
+
+@Component
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	public User findById(Long id) {
+	Optional<User> obj =	repository.findById(id);
+	return obj.get();
+	}
+}
