2276d36a6ac2c997c91b573768c61e7e2787fbcd
Murilo Rodrigues <murilooliveira10789@gmail.com>
2025-03-26 21:19:00 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/ceramicamaster/aprender/services/OrderService.java
+++ src/main/java/com/ceramicamaster/aprender/services/OrderService.java
@@ -6,20 +6,20 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.ceramicamaster.aprender.entities.User;
-import com.ceramicamaster.aprender.repositories.UserRepository;
+import com.ceramicamaster.aprender.entities.Order;
+import com.ceramicamaster.aprender.repositories.OrderRepository;
 
 @Component
-public class UserService {
+public class OrderService {
 
 	@Autowired
-	private UserRepository repository;
+	private OrderRepository repository;
 	
-	public List<User> findAll(){
+	public List<Order> findAll(){
 		return repository.findAll();
 	}
-	public User findById(Long id) {
-	Optional<User> obj = repository.findById(id);
+	public Order findById(Long id) {
+	Optional<Order> obj = repository.findById(id);
 	return obj.get();
 	}
 }
