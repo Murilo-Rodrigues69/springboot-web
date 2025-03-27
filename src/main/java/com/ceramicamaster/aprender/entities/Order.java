e0f731d4c2669468f3690d5bfe53d917f684ea0f
Murilo Rodrigues <murilooliveira10789@gmail.com>
2025-03-26 22:59:28 -0300

    OrderStatus enum

--- src/main/java/com/ceramicamaster/aprender/entities/Order.java
+++ src/main/java/com/ceramicamaster/aprender/entities/Order.java
@@ -4,9 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.ceramicamaster.aprender.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,6 +28,8 @@ public class Order implements Serializable {
 	@JsonFormat(shape= JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'z'", timezone = "GMT")
 	private Instant	moment;
 	
+	private Integer orderStatus;
+	
 	
 	@ManyToOne
 	@JoinColumn(name="client_id")
@@ -38,11 +39,13 @@ public class Order implements Serializable {
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
+		
 	}
 
 	public Long getId() {
@@ -59,7 +62,17 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}	
+	public OrderStatus getOrderStatus() throws IllegalAccessException {
+		return OrderStatus.valueOf(orderStatus) ;
 	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+		this.orderStatus = orderStatus.getCode();
+		}
+		}
+		
 
 	public User getClient() {
 		return client;
