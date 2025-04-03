8c65a0e26be88a62d9277dd426ba9c8e896e0171
Murilo Rodrigues <murilooliveira10789@gmail.com>
2025-04-03 20:30:54 -0300

    Exception handling - findById

--- src/main/java/com/ceramicamaster/aprender/services/UserService.java
+++ src/main/java/com/ceramicamaster/aprender/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import com.ceramicamaster.aprender.entities.User;
 import com.ceramicamaster.aprender.repositories.UserRepository;
+import com.ceramicamaster.aprender.services.exceptions.ResourceNotFoundException;
 
 @Component
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
