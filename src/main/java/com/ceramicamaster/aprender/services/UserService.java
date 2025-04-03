174b7e482fd93a694205ce7faf5d11b12718dd80
Murilo Rodrigues <murilooliveira10789@gmail.com>
2025-04-03 20:52:07 -0300

    Exception handling - update

--- src/main/java/com/ceramicamaster/aprender/services/UserService.java
+++ src/main/java/com/ceramicamaster/aprender/services/UserService.java
@@ -13,6 +13,8 @@ import com.ceramicamaster.aprender.repositories.UserRepository;
 import com.ceramicamaster.aprender.services.exceptions.DataBaseException;
 import com.ceramicamaster.aprender.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Component
 public class UserService {
 
@@ -43,9 +45,14 @@ public class UserService {
 	}
 	
 	public User update (Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity,obj);
 		return repository.save(entity);
+		}
+		catch (EntityNotFoundException e ) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
