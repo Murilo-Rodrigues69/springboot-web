361f08dbf86d141f992e9c720abb5e9c266e4f91
Murilo Rodrigues <murilooliveira10789@gmail.com>
2025-04-02 21:35:47 -0300

    Payment, one-to-one association

--- src/main/java/com/ceramicamaster/aprender/config/TestConfig.java
+++ src/main/java/com/ceramicamaster/aprender/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ceramicamaster.aprender.entities.Category;
 import com.ceramicamaster.aprender.entities.Order;
 import com.ceramicamaster.aprender.entities.OrderItem;
+import com.ceramicamaster.aprender.entities.Payment;
 import com.ceramicamaster.aprender.entities.Product;
 import com.ceramicamaster.aprender.entities.User;
 import com.ceramicamaster.aprender.entities.enums.OrderStatus;
@@ -81,6 +82,12 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		
+		orderRepository.saveAll(Arrays.asList(o1));
 	}
 	 
 }
